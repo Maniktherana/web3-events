9cfc817f03c31f0767ac061229b2e6274b99a7eb
Manik Rana <manikrana54@gmail.com>
2022-04-01 23:53:44 -0400

    Add mongoDB and redux

--- server/index.js
+++ server/index.js
@@ -2,25 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import './config.js'
-import postRoutes from './routes/posts.js'
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes)
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const port = process.env.PORT || 5000
+app.use('/posts', postRoutes);
 
-const CONNECTTION_URL = process.env.MONGODB_URI
+const CONNECTION_URL = '';
+const PORT = process.env.PORT|| 3000;
 
-mongoose.connect(CONNECTTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => {
-		app.listen(port, () => console.log(`Server running on port: ${port}`))
-	})
-	.catch((error) => {
-		console.log(error.message)
-	})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
+
+	

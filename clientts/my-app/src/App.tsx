49221edea08f616bc251c04e54684133b3f8679b
Manik Rana <manikrana54@gmail.com>
2022-04-12 23:28:28 -0400

    Add clientts

---
+++ clientts/my-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Paper } from '@mantine/core';
+import { HeaderMiddle } from './components/Navbar';
+import { linksItems } from './components/mockdata';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import LightDarkButton from './components/LightDarkButton';
+import { useHotkeys, useLocalStorage } from '@mantine/hooks';
+
+
+function App() {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]); 
+
+  return (
+      <div className='App'>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+          <MantineProvider theme={{colorScheme}}>
+            <Paper radius={0} style={{minHeight: "100vh"}}>
+              <HeaderMiddle links={linksItems} />
+            </Paper>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </div>  
+  );
+}
+
+export default App;
+ 
---
+++ clientts/my-app/src/components/mockdata.js
@@ -0,0 +1,15 @@
+export const linksItems = [
+      {
+        "link": "/about",
+        "label": "Home"
+      },
+      {
+        "link": "/learn",
+        "label": "Features"
+      },
+      {
+        "link": "/pricing",
+        "label": "Pricing"
+      }
+    ]
+  
